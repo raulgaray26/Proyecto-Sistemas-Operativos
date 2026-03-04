dbe152a5623190c94e36bf4b7fe40f2df31a88c2
raulgaray26 <raulgaray@uees.edu.ec>
2026-03-03 20:13:09 -0500

    Create info_proyecto.py

---
+++ info_proyecto.py
@@ -0,0 +1,10 @@
+def descripcion_proyecto():
+    titulo = "Laboratorio de Computación"
+    descripcion = "Plataforma que organiza el uso de computadoras en un laboratorio académico, registrando disponibilidad y tiempos de utilización por parte de los estudiantes."
+
+    print("Título del proyecto:")
+    print(titulo)
+    print("\nDescripción del proyecto:")
+    print(descripcion)
+
+descripcion_proyecto()
