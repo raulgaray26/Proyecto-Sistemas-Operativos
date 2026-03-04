1fc4033cbad82a6f3de67b6e86f9d1c0e2938770
raulgaray26 <raulgaray@uees.edu.ec>
2026-03-03 22:35:21 -0500

    Delete info_proyecto.py

--- info_proyecto.py
+++
@@ -1,8 +0,0 @@
-def descripcion_proyecto():
-    titulo = "Laboratorio de Computación"
-    descripcion = "Plataforma que organiza el uso de computadoras en un laboratorio académico, registrando disponibilidad y tiempos de utilización por parte de los estudiantes."
-
-    print("Título del proyecto:")
-    print(titulo)
-    print("\nDescripción del proyecto:")
-    print(descripcion)
