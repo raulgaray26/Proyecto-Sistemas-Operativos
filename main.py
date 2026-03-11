83cfa553e1393cc2fa546ab41f35e244ef01a0c0
raulgaray26 <raulgaray@uees.edu.ec>
2026-03-11 00:06:17 +0000

    main.py

--- main.py
+++ main.py
@@ -8,31 +8,34 @@ def main():
     print("1. Ver nombres de estudiantes")
     print("2. Ver descripción del proyecto")
     print("3. Ejecutar simulación del laboratorio")
+    print("4. Salir")
+    
+    opcion = 0
+    while opcion != 4:
+        opcion = input("Seleccione una opción: ")
 
-    opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            info_estudiantes.nombres_estudiantes()
 
-    if opcion == "1":
-        info_estudiantes.nombres_estudiantes()
+        elif opcion == "2":
+            info_proyecto.descripcion_proyecto()
 
-    elif opcion == "2":
-        info_proyecto.descripcion_proyecto()
+        elif opcion == "3":
+            laboratorio = Laboratorio(3)
 
-    elif opcion == "3":
-        laboratorio = Laboratorio(3)
+            estudiantes = [
+                Estudiante(1, "Leonor", "Investigar en internet"),
+                Estudiante(2, "Raul", "Hacer tarea de Python"),
+                Estudiante(3, "Andrea", "Usar Word"),
+                Estudiante(4, "Carlos", "Preparar exposición"),
+                Estudiante(5, "María", "Revisar correo institucional")
+            ]
 
-        estudiantes = [
-            Estudiante(1, "Leonor", "Investigar en internet"),
-            Estudiante(2, "Raul", "Hacer tarea de Python"),
-            Estudiante(3, "Andrea", "Usar Word"),
-            Estudiante(4, "Carlos", "Preparar exposición"),
-            Estudiante(5, "María", "Revisar correo institucional")
-        ]
+            laboratorio.recibir_estudiantes(estudiantes)
+            laboratorio.abrir()
 
-        laboratorio.recibir_estudiantes(estudiantes)
-        laboratorio.abrir()
-
-    else:
-        print("Opción no válida.")
+        else:
+            print("Opción no válida.")
 
 
 if __name__ == "__main__":
